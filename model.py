9c61a79b77b2ba268c5fa4ae81ef27a8ce502bff
QingYuan Song <471706580@qq.com>
2019-07-11 15:37:58 +0800

    remove istraining

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ from data_load import load_vocab, loadGloVe
 from modules import get_token_embeddings, ff, positional_encoding, multihead_attention, ln, noam_scheme
 from matching import match_passage_with_question, localInference
 
+
 class FI:
     """
     xs: tuple of
@@ -16,7 +17,6 @@ class FI:
         self.token2idx, self.idx2token, self.hp.vocab_size = load_vocab(hp.vocab)
         self.embd = None
         if self.hp.preembedding:
-
             self.embd = loadGloVe(self.hp.vec_path)
         self.embeddings = get_token_embeddings(self.embd, self.hp.vocab_size, self.hp.d_model, zero_pad=False)
         self.x = tf.placeholder(tf.int32, [None, None], name="text_x")
@@ -30,7 +30,6 @@ class FI:
         self.acc = self._acc_op()
         self.global_step = self._globalStep_op()
         self.train = self._training_op()
-
 
     def create_feed_dict(self, x, y, x_len, y_len, truth):
         feed_dict = {
@@ -53,8 +52,7 @@ class FI:
 
         return feed_dict
 
-
-    def representation(self, xs, ys, training=True):
+    def representation(self, xs, ys):
         with tf.variable_scope("representation", reuse=tf.AUTO_REUSE):
             x = xs
             y = ys
@@ -67,29 +65,39 @@ class FI:
             encx *= self.hp.d_model ** 0.5  # scale
 
             encx += positional_encoding(encx, self.hp.maxlen)
-            encx = tf.layers.dropout(encx, self.hp.dropout_rate, training=training)
+            encx = tf.layers.dropout(encx, self.hp.dropout_rate, training=self.hp.is_training)
 
             ency = tf.nn.embedding_lookup(self.embeddings, y)  # (N, T1, d_model)
             ency *= self.hp.d_model ** 0.5  # scale
 
             ency += positional_encoding(ency, self.hp.maxlen)
-            ency = tf.layers.dropout(ency, self.hp.dropout_rate, training=training)
+            ency = tf.layers.dropout(ency, self.hp.dropout_rate, training=self.hp.is_training)
 
-            #add ln
+            # add ln
             encx = ln(encx)
             ency = ln(ency)
 
             ## Blocks
             x_layer = []
             y_layer = []
+            # represention Block
+            for i in range(self.hp.num_extract_blocks):
+                encx = self.base_blocks(encx, scope="num_extract_blocks_{}".format(i))
+                ency = self.base_blocks(ency, scope="num_extract_blocks_{}".format(i))
+
+            # add ln
+            encx = ln(encx)
+            ency = ln(ency)
+
+            # Inference Block
             for i in range(self.hp.inference_blocks):
-                encx, ency = self.inference_blocks(encx, ency, training=training, scope="num_blocks_{}".format(i))
-        #return x_layer, y_layer
+                encx, ency = self.inference_blocks(encx, ency, scope="num_inference_blocks_{}".format(i))
+        # return x_layer, y_layer
         return encx, ency
 
-    #def match_sentences(self, x_steps, t_stepss):
+    # def match_sentences(self, x_steps, t_stepss):
 
-    def inference_blocks(self, a_repre, b_repre, scope, training=True, reuse=tf.AUTO_REUSE):
+    def inference_blocks(self, a_repre, b_repre, scope, reuse=tf.AUTO_REUSE):
         with tf.variable_scope(scope, reuse=reuse):
             # self-attention
             encx = multihead_attention(queries=a_repre,
@@ -97,7 +105,7 @@ class FI:
                                        values=a_repre,
                                        num_heads=self.hp.num_heads,
                                        dropout_rate=self.hp.dropout_rate,
-                                       training=training,
+                                       training=self.hp.is_training,
                                        causality=False)
             # feed forward
             encx = ff(encx, num_units=[self.hp.d_ff, self.hp.d_model])
@@ -108,7 +116,7 @@ class FI:
                                        values=b_repre,
                                        num_heads=self.hp.num_heads,
                                        dropout_rate=self.hp.dropout_rate,
-                                       training=training,
+                                       training=self.hp.is_training,
                                        causality=False)
             # feed forward
             ency = ff(ency, num_units=[self.hp.d_ff, self.hp.d_model])
@@ -117,13 +125,12 @@ class FI:
 
             return encx, ency
 
-
     def _infer(self, encx, ency, scope="local_inference"):
         with tf.variable_scope(scope):
-            x_mask = tf.sequence_mask(self.x_len, self.hp.maxlen, dtype=tf.float32)
-            y_mask = tf.sequence_mask(self.y_len, self.hp.maxlen, dtype=tf.float32)
-            match_result_x = match_passage_with_question(encx, ency, x_mask, y_mask)
-            match_result_y = match_passage_with_question(ency, encx, x_mask, y_mask)
+            # x_mask = tf.sequence_mask(self.x_len, self.hp.maxlen, dtype=tf.float32)
+            # y_mask = tf.sequence_mask(self.y_len, self.hp.maxlen, dtype=tf.float32)
+            # match_result_x = match_passage_with_question(encx, ency, x_mask, y_mask)
+            # match_result_y = match_passage_with_question(ency, encx, x_mask, y_mask)
 
             attentionWeights = tf.matmul(encx, tf.transpose(ency, [0, 2, 1]))
             attentionSoft_a = tf.nn.softmax(attentionWeights)
@@ -138,14 +145,12 @@ class FI:
             b_diff = tf.subtract(ency, b_hat)
             b_mul = tf.multiply(ency, b_hat)
 
-            a_res = tf.concat([a_hat, a_diff, a_mul, match_result_x], axis=2)
-            b_res = tf.concat([b_hat, b_diff, b_mul, match_result_y], axis=2)
-
-
+            a_res = tf.concat([a_hat, a_diff, a_mul], axis=2)
+            b_res = tf.concat([b_hat, b_diff, b_mul], axis=2)
 
             # BN
-            #a_res = tf.layers.batch_normalization(a_res, training=self.hp.is_training, name='bn1', reuse=tf.AUTO_REUSE)
-            #b_res = tf.layers.batch_normalization(b_res, training=self.hp.is_training, name='bn2', reuse=tf.AUTO_REUSE)
+            # a_res = tf.layers.batch_normalization(a_res, training=self.hp.is_training, name='bn1', reuse=tf.AUTO_REUSE)
+            # b_res = tf.layers.batch_normalization(b_res, training=self.hp.is_training, name='bn2', reuse=tf.AUTO_REUSE)
             # project
             a_res = self._project_op(a_res)  # (?,?,d_model)
             b_res = self._project_op(b_res)  # (?,?,d_model)
@@ -167,22 +172,21 @@ class FI:
 
             return inputx
 
-
-    def base_blocks(self, a_repre, b_repre, scope, training=True, reuse=tf.AUTO_REUSE):
+    def base_blocks(self, a_repre, scope, reuse=tf.AUTO_REUSE):
         with tf.variable_scope(scope, reuse=reuse):
             # self-attention
             encx = multihead_attention(queries=a_repre,
-                                       keys=b_repre,
-                                       values=b_repre,
+                                       keys=a_repre,
+                                       values=a_repre,
                                        num_heads=self.hp.num_heads,
                                        dropout_rate=self.hp.dropout_rate,
-                                       training=training,
+                                       training=self.hp.is_training,
                                        causality=False)
             # feed forward
             encx = ff(encx, num_units=[self.hp.d_ff, self.hp.d_model])
         return encx
 
-    def inter_blocks(self, a_repre, b_repre, scope, training=True, reuse=tf.AUTO_REUSE):
+    def inter_blocks(self, a_repre, b_repre, scope, reuse=tf.AUTO_REUSE):
         with tf.variable_scope(scope, reuse=reuse):
             # self-attention
             encx = multihead_attention(queries=b_repre,
@@ -190,7 +194,7 @@ class FI:
                                        values=a_repre,
                                        num_heads=self.hp.num_heads,
                                        dropout_rate=self.hp.dropout_rate,
-                                       training=training,
+                                       training=self.hp.is_training,
                                        causality=False)
             # feed forward
             encx = ff(encx, num_units=[self.hp.d_ff, self.hp.d_model])
@@ -201,15 +205,14 @@ class FI:
                                        values=b_repre,
                                        num_heads=self.hp.num_heads,
                                        dropout_rate=self.hp.dropout_rate,
-                                       training=training,
+                                       training=self.hp.is_training,
                                        causality=False)
             # feed forward
             ency = ff(ency, num_units=[self.hp.d_ff, self.hp.d_model])
 
-
         return encx, ency
 
-    def aggregation(self, a_repre, b_repre, training=True):
+    def aggregation(self, a_repre, b_repre):
         dim = a_repre.shape.as_list()[-1]
         with tf.variable_scope("aggregation", reuse=tf.AUTO_REUSE):
             # Blocks
@@ -217,13 +220,13 @@ class FI:
                 with tf.variable_scope("num_blocks_{}".format(i), reuse=tf.AUTO_REUSE):
                     # Vanilla attention
                     a_repre = multihead_attention(queries=a_repre,
-                                              keys=a_repre,
-                                              values=a_repre,
-                                              num_heads=self.hp.num_heads,
-                                              dropout_rate=self.hp.dropout_rate,
-                                              training=training,
-                                              causality=False,
-                                              scope="vanilla_attention")
+                                                  keys=a_repre,
+                                                  values=a_repre,
+                                                  num_heads=self.hp.num_heads,
+                                                  dropout_rate=self.hp.dropout_rate,
+                                                  training=self.hp.is_training,
+                                                  causality=False,
+                                                  scope="vanilla_attention")
                     ### Feed Forward
                     a_repre = ff(a_repre, num_units=[self.hp.d_ff, dim])
         return a_repre
@@ -248,7 +251,7 @@ class FI:
             accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32), name='Accuracy')
         return accuracy
 
-    #将transformer的每层作为一个channel输入CNN
+    # 将transformer的每层作为一个channel输入CNN
     def cnn_agg(self, match_channels):
         # Create a convolution + maxpool layer for each filter size
         filter_sizes = list(map(int, self.hp.filter_sizes.split(",")))
@@ -291,7 +294,7 @@ class FI:
 
     def _logits_op(self):
         # representation
-        x_repre, y_repre = self.representation(self.x, self.y) #(layers, batchsize, maxlen, d_model)
+        x_repre, y_repre = self.representation(self.x, self.y)  # (layers, batchsize, maxlen, d_model)
 
         # BN
         x_repre = tf.layers.batch_normalization(x_repre, training=self.hp.is_training, name='bn1', reuse=tf.AUTO_REUSE)
@@ -330,9 +333,9 @@ class FI:
 
     def _training_op(self):
         # train scheme
-        #global_step = tf.train.get_or_create_global_step()
+        # global_step = tf.train.get_or_create_global_step()
         optimizer = tf.train.AdamOptimizer(self.hp.lr)
-        #optimizer = tf.train.AdadeltaOptimizer(lr)
+        # optimizer = tf.train.AdadeltaOptimizer(lr)
 
         '''
         if self.hp.lambda_l2>0.0:
@@ -352,16 +355,15 @@ class FI:
 
     def predict_model(self):
         # representation
-        x_repre, y_repre = self.representation(self.x, self.y, False) # (batchsize, maxlen, d_model)
+        x_repre, y_repre = self.representation(self.x, self.y)  # (batchsize, maxlen, d_model)
         x_mask = tf.sequence_mask(self.x_len, self.hp.maxlen, dtype=tf.float32)
         y_mask = tf.sequence_mask(self.y_len, self.hp.maxlen, dtype=tf.float32)
 
-
         # matching
-        match_result = self.match_passage_with_question(x_repre, y_repre, x_mask, y_mask)
+        match_result = match_passage_with_question(x_repre, y_repre, x_mask, y_mask)
 
         # aggre
-        x_inter = self.aggregation(match_result, match_result, False)  # (?, ?, 512)
+        x_inter = self.aggregation(match_result, match_result)  # (?, ?, 512)
 
         x_avg = tf.reduce_mean(x_inter, axis=1)
         x_max = tf.reduce_max(x_inter, axis=1)
@@ -373,8 +375,3 @@ class FI:
             label_pred = tf.argmax(logits, 1, name='label_pred')
 
         return label_pred
-
-
-
-
-
